03a0205aec9daf51d0b8f5fb3582fbf9de200b44
Alejandro Muriel <amuripra@myuax.com>
2026-03-05 09:17:27 +0100

    test (Main) se ha testeado el funcionamiento correcto del codigo,y se ha encontrado un erro que ya ha sido actualizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
 
 void main() {
 
+    Alumno alumno1;
+    Alumno alumno2;
+    Alumno alumno3;
+
+    CentroFP centroFP;
+
+    alumno1 = new Alumno(1, "Carlos", "DAM", 21);
+    alumno2 = new Alumno(2, "Ivan", "ASIR", 18);
+    alumno3 = new Alumno(3, "Ana", "DAM", 19);
+
+    centroFP = new CentroFP(50);
+
+    System.out.println(centroFP.registrarAlumno(alumno1));
+    System.out.println(centroFP.registrarAlumno(alumno1));
+    System.out.println(centroFP.registrarAlumno(alumno3));
+    System.out.println(centroFP.registrarAlumno(alumno2));
+
+    centroFP.imprimirAlumnos();
 }
