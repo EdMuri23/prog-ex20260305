01597b6c70502d8d0da0587d4a10c658b5e9560b
Alejandro Muriel <amuripra@myuax.com>
2026-03-05 08:49:42 +0100

    feat (Alumno) se ha creado la clase en su totalidad, con sus atributos en privado, su constructor, getters y su toString

---
+++ src/Alumno.java
@@ -0,0 +1,48 @@
+public class Alumno {
+
+    //Atributos
+    private int id;
+    private String nombre;
+    private String tituloFP;
+    private int edad;
+
+    /**CONSTRUCTOR_ALUMNO
+     *
+     * @param id
+     * @param nombre
+     * @param tituloFP
+     * @param edad
+     */
+    public Alumno(int id, String nombre, String tituloFP, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTituloFP() {
+        return tituloFP;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    //ToString
+    public String toString() {
+        StringBuilder infoAlum = new StringBuilder("Alumno: \t");
+        infoAlum.append(id + "\n\t");
+        infoAlum.append("nombre:" + nombre + "\n\t");
+
+        return infoAlum.toString();
+    }
+}
